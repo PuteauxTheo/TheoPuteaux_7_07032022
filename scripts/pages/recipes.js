45d04e38f2c525e490807b08419da9490a20877f
Theo Puteaux <theo.ptx@laposte.net>
2022-03-31 14:57:16 +0200

    remplacement des forEach par des for

--- scripts/pages/recipes.js
+++ scripts/pages/recipes.js
@@ -1,11 +1,11 @@
 const divRecipes = document.getElementById('recipes');
 
 //displayRecipes permet d'afficher les recettes sur la page
-function displayRecipes(dataRecipes){
+function displayRecipes(dataRecipes){   
 
-    dataRecipes.forEach( el => {
-        const a = recipesFactory(el)
-        const recipesDOM = a.getRecipesDOM();
+    for(let i=0; i < dataRecipes.length;i++){
+        const recipesModel = recipesFactory(dataRecipes[i])
+        const recipesDOM = recipesModel.getRecipesDOM();
         divRecipes.appendChild(recipesDOM);
-    })   
+    }
 }
