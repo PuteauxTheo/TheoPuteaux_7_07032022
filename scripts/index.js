b9c052eb3468e2cbced4eb620edb49dd0d2285cc
Theo Puteaux <theo.ptx@laposte.net>
2022-03-31 14:58:00 +0200

    remplacement des forEach par des for

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,66 @@
-// for(let y =0; y < recipes.length; y++){
-//     for(let i =0;i < recipes[y].ingredients.length;i++){
-        
-//         tabIngredients.push(recipes[y].ingredients[i].ingredient) 
-//         // console.log(" tab a chaque fois "+tabIngredients)
-
-//     }
-// }
-
-// tabDataTag permet de creer les tableaux pour les filtres 
-
 // tabIngredient renvoie le tableau de tous les ingredients sans doublons 
-function tabIngredient(data){
-    let tabDataIngredients = [];
 
-    data.forEach( el => {
-        el.ingredients.forEach( ing => {
-            tabDataIngredients.push(ing.ingredient.toLowerCase())
-        })
-    })
-    return (tabDataIngredients.filter(isUniqueValue))
+function tabIngredient(data){
+    let tabDataIngredients = []
+
+    for(let i=0; i < data.length; i++){
+        for(let j=0; j < data[i].ingredients.length; j++){
+            tabDataIngredients.push(data[i].ingredients[j].ingredient.toLowerCase());
+        }
+    }
+    //cette boucle permet d'enlever les doublons 
+    for(let l=0; l < tabDataIngredients.length; l++){
+        for(let m=0; m < tabDataIngredients.length; m++){
+            if(tabDataIngredients[l] == tabDataIngredients[m]){
+                tabDataIngredients.splice(l,1);
+            }
+        }
+    }
+    return (tabDataIngredients)
 }
 
 // tabAppareil renvoie le tableau de tous les ingredients sans doublons 
 
 function tabAppareil(data){
-     let tabDataAppareils = []
-    data.forEach( el => {
-            tabDataAppareils.push(el.appliance.toLowerCase())
-    })
-    return (tabDataAppareils.filter(isUniqueValue))
+    let tabDataAppareils = [];
+
+    for(let i = 0; i < data.length; i++){
+        tabDataAppareils.push(data[i].appliance.toLowerCase())
+    }
+    // cette boucle permet d'enlever les doublons 
+    for(let l=0; l < tabDataAppareils.length; l++){
+        for(let m=0; m < tabDataAppareils.length; m++){
+            if(tabDataAppareils[l] == tabDataAppareils[m]){
+                tabDataAppareils.splice(l,1);
+            }
+        }
+    }
+
+    return (tabDataAppareils)
 }
 
 // tabUstensil renvoie le tableau de tous les ingredients sans doublons 
 
 function tabUstensil(data){
     let tabDataUstensils = []
-    data.forEach( el => {
-        el.ustensils.forEach( ust => {
-            tabDataUstensils.push(ust.toLowerCase())
-        })
-    })
-    return (tabDataUstensils.filter(isUniqueValue))
+    console.log("tabDataUs"+tabDataUstensils)
+    for(let i=0; i < data.length; i++){
+        for(let j=0; j < data[i].ustensils.length; j++){               
+                tabDataUstensils.push(data[i].ustensils[j].toLowerCase());
+        }    
+    }
+    // cette boucle permet d'enlever les doublons 
+    for(let l=0; l < tabDataUstensils.length; l++){
+        for(let m=0; m < tabDataUstensils.length; m++){
+            if(tabDataUstensils[l] == tabDataUstensils[m]){
+                tabDataUstensils.splice(l,1);
+            }
+        }
+    }
+    return (tabDataUstensils)
+
 }
+
 
 var Ingredient = tabIngredient(recipes);
 var Appareil = tabAppareil(recipes);
