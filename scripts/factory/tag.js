e709f7e934ef3c901c33ce04241922cea1385e2a
Theo Puteaux <theo.ptx@laposte.net>
2022-04-04 22:17:21 +0200

    appel de addTagSelected au click des tags

--- scripts/factory/tag.js
+++ scripts/factory/tag.js
@@ -3,8 +3,11 @@ function tagFactory(data, cat){
     const span = document.createElement('span')
     span.className = "tag-list"
     span.textContent = data;
-    span.addEventListener('click', () => {
-        displayTag(data, cat)
+    // span.addEventListener('click', () => {
+    //     displayTag(data, cat)
+    // })
+    span.addEventListener('click', () =>{
+        addTagSelected(data, cat);
     })
 
     return (span)
